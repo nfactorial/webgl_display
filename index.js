66da578415bce5d083242986bd6e3056f612828d
nfactorial <nfactorial@btopenworld.com>
2018-09-01 16:01:10 +0100

    Updated to latest webgl helper library, material is now correctly exported.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import * as Math from './lib/math';
+import Material from './lib/material';
 import Renderer from './lib/renderer';
 import DisplayPort from './lib/display_port';
 import * as FullScreen from './lib/fullscreen';
 import {CameraType} from './lib/camera_args';
 import CameraArgs from './lib/camera_args';
 import DrawRequest from './lib/draw_request';
+import MaterialInstance from './lib/material/instance';
 import * as PipelineProvider from './lib/pipeline_provider';
 
 export {
@@ -15,5 +17,7 @@ export {
     CameraType,
     CameraArgs,
     DrawRequest,
+    Material,
+    MaterialInstance,
     PipelineProvider
 };
