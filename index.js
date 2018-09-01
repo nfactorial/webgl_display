07a6e648fdfc62688db1c5c723c3412c42dc1c60
nfactorial <nfactorial@btopenworld.com>
2018-09-01 15:44:57 +0100

    Updated to latest WebGLHelper, PipelineProvider is now exported correctly.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import DisplayPort from './lib/display_port';
 import * as FullScreen from './lib/fullscreen';
 import {CameraType} from './lib/camera_args';
 import CameraArgs from './lib/camera_args';
+import DrawRequest from './lib/draw_request';
 import * as PipelineProvider from './lib/pipeline_provider';
 
 export {
@@ -13,5 +14,6 @@ export {
     DisplayPort,
     CameraType,
     CameraArgs,
+    DrawRequest,
     PipelineProvider
 };
